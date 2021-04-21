a41fc843f29bbd9fefe095cf86680aabcffaa4a8
JORGE AGUIRREGOMEZCORTA AINA <a2071@inycom.es>
2021-04-21 17:20:52 +0200

    Correct Accent Comparison

--- EasyScholarRanking/Data/AuthorFileService.cs
+++ EasyScholarRanking/Data/AuthorFileService.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
+using System.Text;
+using System.Globalization;
 
 namespace EasyScholarRanking.Data
 {
@@ -17,15 +19,37 @@ namespace EasyScholarRanking.Data
         public static bool SearchAuthor(string authorName)
         {
             authorName = authorName.ToLower();
+            authorName = RemoveDiacritics(authorName);
+
             foreach (string line in File.ReadLines(FileName))
             {
-                if (line.ToLower().Equals(authorName))
+                string currentLine = line.ToLower();
+                currentLine = RemoveDiacritics(currentLine);
+
+                if (currentLine.Equals(authorName))
                 {
                     return true;
                 }
             }
 
             return false;
+        }
+
+        private static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
     }
 }
